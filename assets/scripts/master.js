8b343de739d2a9e0d706f17af684b43b00d342f0
Christian Abdelmassih <chrabd@kth.se>
2017-09-27 18:19:06 +0200

    add browserify to gulp

---
+++ assets/scripts/master.js
@@ -0,0 +1,8 @@
+var swal = require('sweetalert');
+var $ = require('jquery');
+
+
+$(document).ready(function() {
+  console.log('RAN!!!! :D');
+  swal ( "Oops" ,  "Something went wrong!" ,  "error" )
+})
--- gulpfile.js
+++ gulpfile.js
@@ -21,23 +21,19 @@ var concat = require('gulp-concat');
 var del = require('del');
 var minify = require('gulp-minify');
 var pump = require('pump');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 // for CSS
 var sass = require('gulp-sass');
 var cssnano = require('gulp-cssnano');
 var stripCssComments = require('gulp-strip-css-comments');
-var sourcemaps = require('gulp-sourcemaps');
 
-// for server
-var lr = require('tiny-lr')();
-
-var refresh = function(event) {
-  var fileName = require('path').relative(__dirname, event.path);
-  gutil.log.apply(gutil, [gutil.colors.magenta(fileName), gutil.colors.cyan('built')]);
-  lr.changed({
-    body: { files: [fileName] }
-  });
-}
+// for JS
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
+var buffer = require('vinyl-buffer');
+var reactify = require('reactify');
 
 // *************************** IMPORTS ***********************************
 
@@ -55,7 +51,7 @@ gulp.task('compile-sass', function() {
 gulp.task('concat-css',['compile-sass'], function() {
   return gulp.src([
     'public/master.sass.css',
-    'node_modules/sweetalert/src/sweetalert.css',
+    //'node_modules/sweetalert/src/sweetalert.css',
   ])
   .pipe(concat('master.css'))
   .pipe(gulp.dest('public'));
@@ -76,60 +72,41 @@ gulp.task('minify-css',['compile-sass', 'concat-css'], function() {
 
 // **************************** JS  *******************************************
 
-gulp.task('concat-js', function() {
-  // ADD ALL BOWER JS FILES HERE
-  return gulp.src([
-      // 'bower_components/jquery/dist/jquery.min.js',
-      // 'bower_components/scrollreveal/dist/scrollreveal.min.js',
-      'node_modules/sweetalert/dist/sweetalert.min.js',
-      // 'bower_components/typed.js/dist/typed.min.js',
-      // 'bower_components/velocity/velocity.min.js',
-      `${SCRIPT_PATH}/*.js`
-    ])
-    .pipe(concat('master.js'))
-    .pipe(gulp.dest('public'));
-});
+gulp.task('javascript', function () {
+  // set up the browserify instance on a task basis
+  var b = browserify({
+    entries: 'assets/scripts/master.js',
+    debug: true,
+    // defining transforms here will avoid crashing your stream
+    transform: [reactify]
+  });
 
-gulp.task('minify-js', ['concat-js'], function(cb) {
-  pump([
-    gulp.src('public/master.js'),
-    minify({ext: {src:'.js',min:'.min.js'}}),
-    gulp.dest('public')
-  ], cb);
-  // return gulp.src('public/master.js')
-  // .pipe(minify({
-  //     ext: {
-  //         src:'.js',
-  //         min:'.min.js'
-  //     }
-  // }))
-  // .pipe(gulp.dest('public'));
+  return b.bundle()
+    .pipe(source('master.min.js'))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    // Add transformation tasks to the pipeline here.
+    .pipe(uglify())
+    .on('error', gutil.log)
+    .pipe(gulp.dest('./public/'));
 });
 
 // **************************** JS  *******************************************
 
 // **************************** GULP TASKS  ***********************************
 
-gulp.task('serve', function () {
-  app = require('./server.js');
-  lr.listen(35729);
-
-  gulp.src('')
-  .pipe(open({uri: 'http://localhost:3000'}));
-});
-
-gulp.task('cleanup',['minify-css', 'minify-js'], function() {
+gulp.task('cleanup',['minify-css'], function() {
   del('public/master.js');
   del('public/master.css');
   del('public/master.sass.css');
 });
 
 gulp.task('styles', ['compile-sass', 'concat-css', 'minify-css']);
-gulp.task('scripts', ['concat-js', 'minify-js']);
-gulp.task('default', ['styles', 'scripts', 'cleanup', 'serve', 'watch']);
+gulp.task('scripts', ['javascript']);
+gulp.task('default', ['styles', 'scripts', 'cleanup', 'watch']);
 
 gulp.task('watch', function() {
   gulp.watch(`${SASS_PATH}/*.sass`, ['styles', 'cleanup']);
   gulp.watch(['bower_components/*', `${SCRIPT_PATH}/*.js`], ['scripts', 'cleanup']);
-  gulp.watch(['public/master.html', 'public/master.min.js', 'public/master.min.css'], refresh);
+  gulp.watch(['public/master.html', 'public/master.min.js', 'public/master.min.css']);
 });
